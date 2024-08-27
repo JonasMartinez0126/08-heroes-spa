3410dcccb9e5766b988aa1d5375023c5ed9324c5
JonasMartinez0126 <127220073+JonasMartinez0126@users.noreply.github.com>
2024-08-27 17:55:03 -0400

    Se comenzo con el componente de HeroCard

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,17 @@
+
+
+
+export const HeroCard = ({ 
+    id,
+    superhero, 
+    publisher, 
+    alter_ego,
+    first_appearance,
+    characters,
+}) => {
+  return (
+    <div className="col">
+        
+    </div>
+  )
+}
